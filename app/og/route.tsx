c50aadadcd620304833b1ed91a09125436b19e5e
Dollengo <131563888+Dollengo@users.noreply.github.com>
2024-08-29 19:13:06 -0300

    Update route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from 'next/og'
 
 export function GET(request: Request) {
   let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Dollengo Studios'
+  let title = url.searchParams.get('title') || 'Dollengo '
 
   return new ImageResponse(
     (
